47e03f066d608d039068802f64bef39023e39435
Paul FLeming <pfleming@heroku.com>
2020-10-18 12:33:06 +0100

    simple logging

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ app = flask.Flask(__name__)
 
 env = Env()
 env.read_env()
+development = os.environ.get('DEVELOPMENT')
 
 # get PORT from os, or use 5000 if not set
 port = int(os.environ.get('PORT', 5000))
 
 @app.route('/', methods=['GET'])
 def home():
+    print(f"Development environment = {development}", flush=True)
+
     response = request.args['request']
     try:
         return "Your request is: " + response
+        
     except KeyError:
         return f'Invalid request'
         
 
-print(f"App listening on Port: {port}", flush=True )
+print(f"App booted succesfully on Port: {port}", flush=True )
