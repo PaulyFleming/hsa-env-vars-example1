e7d25cf72c1aa6eeee54117e70eb403bda3a0dab
Paul FLeming <pfleming@heroku.com>
2020-10-18 11:11:09 +0100

    added PORT logging

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import flask
 from flask import request
+from environs import Env
+import os
 
 app = flask.Flask(__name__)
+
+env = Env()
+env.read_env()
+
+# get PORT from os, or use 5000 if not set
+port = int(os.environ.get('PORT', 5000))
 
 @app.route('/', methods=['GET'])
 def home():
@@ -11,3 +19,5 @@ def home():
     except KeyError:
         return f'Invalid request'
         
+
+print(f"App listening on Port: {port}", flush=True )
