2ba37f0c21f408fededa917156d4590f2e1b11cc
Paul FLeming <pfleming@heroku.com>
2020-10-19 16:11:10 +0100

    updated test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,8 @@
-from flask import Flask
+import flask
 
-def test_base_route():
-    app = Flask(__name__)
-    client = app.test_client()
-    url = '/'
-    
-    response = client.get(url)
-    assert response.get_data() == b'Hello world!'
-    assert response.status_code == 200
+app = flask.FLask(__name__)
+
+def test_base_route('/'):
+
+    assert flask.request.path == '/''
     
